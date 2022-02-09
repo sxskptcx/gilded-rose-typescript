137a141bc703d867b11bf25806de610c50ecd7bc
Adib Zidan <94Amsz@gmail.com>
2022-02-09 17:01:36 +0100

    Adjust concert quality and isLegendary item

--- src/gilded_rose.ts
+++ src/gilded_rose.ts
@@ -7,6 +7,11 @@ const SULFURAS = 'Sulfuras, Hand of Ragnaros';
 const MAX_QUALITY = 50;
 const MIN_QUALITY = 0;
 
+const MAX_CONCERT_SELL_IN = 11;
+const MIN_CONCERT_SELL_IN = 6;
+
+const MIN_SELL_IN_QUALITY = 0;
+
 const incrementQuality = (item: Item): void => {
   if (item.quality < MAX_QUALITY) {
     item.quality = item.quality + 1;
@@ -19,6 +24,28 @@ const decrementQuality = (item: Item): void => {
   }
 };
 
+const isCommonItem = (item: Item): boolean => {
+  return item.name !== AGED_BRIE && item.name !== CONCERT_TICKET && item.name !== SULFURAS;
+};
+
+const adjustConcertQuality = (item: Item): void => {
+  if (item.sellIn < MAX_CONCERT_SELL_IN) {
+    incrementQuality(item);
+  }
+
+  if (item.sellIn < MIN_CONCERT_SELL_IN) {
+    incrementQuality(item);
+  }
+};
+
+const isExpired = (item: Item): boolean => {
+  return item.sellIn < MIN_SELL_IN_QUALITY;
+};
+
+const isLegendary = (item: Item): boolean => {
+  return item.name === SULFURAS;
+};
+
 export class Shop {
 
   public items: Item[] = [];
@@ -29,29 +56,22 @@ export class Shop {
 
   public updateQuality(): Item[] {
     this.items.forEach((item: Item) => {
-      if (item.name !== AGED_BRIE && item.name !== CONCERT_TICKET && item.name !== SULFURAS) {
+      if (isCommonItem(item)) {
         decrementQuality(item);
       } else {
         incrementQuality(item);
-
-        // CONCERT_TICKET
-        if (item.name == CONCERT_TICKET) {
-          if (item.sellIn < 11) {
-            incrementQuality(item);
-          }
-
-          if (item.sellIn < 6) {
-            incrementQuality(item);
-          }
-        }
       }
 
-      if (item.name !== SULFURAS) {
+      if (item.name === CONCERT_TICKET) {
+        adjustConcertQuality(item);
+      }
+
+      if (!isLegendary(item)) {
         item.sellIn = item.sellIn - 1;
       }
 
       // When expired
-      if (item.sellIn < 0) {
+      if (isExpired(item)) {
         if (item.name !== AGED_BRIE) {
           if (item.name !== CONCERT_TICKET) {
             if (item.name !== SULFURAS) {
