677c5e2199e628127a334285c7e76c4dde095eca
Manu Kapoor <mkapoor@tillster.com>
2022-02-10 20:11:03 +0530

    text: improve test names

--- src/__tests__/gilded_rose_spec.ts
+++ src/__tests__/gilded_rose_spec.ts
@@ -1,29 +1,19 @@
 import {Shop, Item} from '../gilded_rose';
 
-const itemsGlobal = [];
-
-itemsGlobal.push(new Item('+5 Dexterity Vest', 10, 20));
-itemsGlobal.push(new Item('Aged Brie', 2, 0));
-itemsGlobal.push(new Item('Elixir of the Mongoose', 5, 7));
-itemsGlobal.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-itemsGlobal.push(new Item('Sulfuras, Hand of Ragnaros', -1, 80));
-itemsGlobal.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-itemsGlobal.push(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49));
-itemsGlobal.push(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49));
-
-
 const MIN_QUALITY = 0;
 const MAX_QUALITY = 50;
+
 describe("Gilded Rose", () => {
-    it("doesnt fail if shop", () => {
+    it("doesnt fail if shop is empty", () => {
         const gildedRose = new Shop();
 
         const updatedItems = gildedRose.updateQuality();
 
         expect(updatedItems).toHaveLength(0);
     });
+
     describe('Regular item', () => {
-        it("should decrease sellin by 1 of regular item if quality>0", () => {
+        it("decreases sellin by 1 of regular item if quality>0", () => {
             const gildedRose = new Shop([new Item("foo", 0, 0)]);
 
             const items = gildedRose.updateQuality();
@@ -48,7 +38,7 @@ describe("Gilded Rose", () => {
             expect(updatedItems[0].quality).toEqual(initialQuality - 2);
         });
 
-        it("should not increase quality of regular item if it's already 50", () => {
+        it("doesnt increase quality of regular item if it's already 50", () => {
             const gildedRose = new Shop([new Item("foo", 1, MAX_QUALITY)]);
 
             const updatedItems = gildedRose.updateQuality();
@@ -67,7 +57,7 @@ describe("Gilded Rose", () => {
     })
 
     describe('Aged Brie', () => {
-        it("should decrease sellin by 1 of Aged Brie and increase quality by 1 if <50", () => {
+        it("decreases sellin by 1 of Aged Brie and increase quality by 1 if <50", () => {
             const gildedRose = new Shop([new Item('Aged Brie', 2, 0)]);
 
             const updatedItems = gildedRose.updateQuality();
@@ -75,7 +65,7 @@ describe("Gilded Rose", () => {
             expect(updatedItems[0].quality).toEqual(1);
         });
 
-        it("should increase quality of Aged Brie by 2 if expired", () => {
+        it("increases quality of Aged Brie by 2 if expired", () => {
             const gildedRose = new Shop([new Item('Aged Brie', -1, 0)]);
 
             const updatedItems = gildedRose.updateQuality();
@@ -83,7 +73,7 @@ describe("Gilded Rose", () => {
             expect(updatedItems[0].quality).toEqual(2);
         });
 
-        it("should not increase quality of Aged Brie if it's already 50", () => {
+        it("doesnt increase quality of Aged Brie if it's already 50", () => {
             const gildedRose = new Shop([new Item('Aged Brie', -1, MAX_QUALITY)]);
 
             const updatedItems = gildedRose.updateQuality();
