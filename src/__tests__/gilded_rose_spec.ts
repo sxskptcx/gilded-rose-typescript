5abfb7785da90b7e2665d36e4fd6d042267cc855
sxcr <heyiamrobot@gmail.com>
2025-09-09 10:07:00 +0200

    fffddb

--- src/__tests__/gilded_rose_spec.ts
+++ src/__tests__/gilded_rose_spec.ts
@@ -1,11 +1,16 @@
 import { Shop, Item } from '../gilded_rose';
 
 describe("Gilded Rose", () => {
-
-    it("should foo", () => {
-        const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    it("ConjuredQualityCheck", () => {
+        const gildedRose = new Shop([new Item('Conjured Mana Cake', 3, 6)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual("fixme");
+
+        expect(gildedRose.items[0].quality).toEqual(2);
     });
 
+    it("RegularItemCheck", () => {
+        const regular = new Item('+5 Dexterity Vest', 10, 20);
+
+        expect(regular.quality).toEqual(18);
+    });
 });
